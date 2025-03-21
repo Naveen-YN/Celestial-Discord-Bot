c1c7f3ccc526d3ee992b6c85072bc27c23814f7c
Naveen Y <naveenyd06@gmail.com>
2025-03-21 13:22:55 +0530

    add data

--- src/commands/utility/avatar.js
+++ src/commands/utility/avatar.js
@@ -6,14 +6,17 @@ module.exports = {
         .setDescription('Get the avatar of a user')
         .addUserOption(option =>
             option.setName('user')
-                .setDescription('The 👤user to get the avatar from ')),
+                .setDescription('The 👤user to get the avatar from')),
 
     async execute(interaction) {
         const user = interaction.options.getUser('user') ?? interaction.user;
 
+        // Generate a random color
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
         const embed = new EmbedBuilder()
             .setTitle(`${user.username}'s Avatar`)
-            .setColor(0x0099FF)
+            .setColor(`#${randomColor}`)
             .setImage(user.displayAvatarURL({ size: 1024, dynamic: true }))
             .addFields(
                 { name: 'Avatar URL', value: `[Click here](${user.displayAvatarURL({ size: 1024, dynamic: true })})` }
